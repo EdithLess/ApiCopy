97d1f0e1b587849c48bb4986231158a4ce0dac4d
Gregory <gregory20korol20@gmail.com>
2024-08-18 17:17:48 +0300

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000
 
 const productsRouter=require("./routes/products")
 const categoriesRouter=require("./routes/categories")
